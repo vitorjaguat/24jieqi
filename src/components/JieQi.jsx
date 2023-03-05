7094093559388b047c723d32825517b3aad31e10
Vitor Butkus <vitorbutkus@gmail.com>
2023-03-05 14:19:33 -0300

    complete first draft of the page design

--- src/components/JieQi.jsx
+++ src/components/JieQi.jsx
@@ -1,6 +1,8 @@
 import { Solar, Lunar } from 'lunar-javascript';
+import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { BiSkipPrevious, BiSkipNext } from 'react-icons/bi';
+import { GiFlowerPot } from 'react-icons/gi';
 
 const DUMMY = {
   立春: {
@@ -21,7 +23,7 @@ const DUMMY = {
 };
 
 export default function JieQi() {
-  const [baseDate, setBaseDate] = useState(new Date());
+  const [baseDate, setBaseDate] = useState(null);
   const [currentJieQi, setCurrentJieQi] = useState(null);
   const [prevSentence, setPrevSentence] = useState('');
   const [nextSentence, setNextSentence] = useState('');
@@ -34,6 +36,7 @@ export default function JieQi() {
   //   };
 
   const prevJieQiHelper = (date) => {
+    if (!date) return;
     const x = Lunar.fromDate(
       new Date(Lunar.fromDate(date).getPrevJieQi().getSolar().next(-1))
     ).getPrevJieQi();
@@ -41,6 +44,7 @@ export default function JieQi() {
   };
 
   const add3DayHelper = (date) => {
+    if (!date) return;
     const dateCopy = new Date(date);
     dateCopy.setDate(date.getDate() + 3);
     return dateCopy;
@@ -51,6 +55,7 @@ export default function JieQi() {
       new Date(Lunar.fromDate(new Date()).getPrevJieQi().getSolar().toYmd())
     );
     setCurrentJieQi(Lunar.fromDate(new Date()).getPrevJieQi().getName());
+    if (!baseDate) return;
     let prev = prevJieQiHelper(baseDate);
     setPrevSentence(prev.getName() + ' ' + prev.getSolar().toYmd());
     let next = Lunar.fromDate(baseDate).getNextJieQi();
@@ -79,6 +84,7 @@ export default function JieQi() {
   };
 
   useEffect(() => {
+    if (!baseDate) return;
     let prev = prevJieQiHelper(add3DayHelper(baseDate));
     setPrevSentence(prev.getName() + ' ' + prev.getSolar().toYmd());
     let next = Lunar.fromDate(add3DayHelper(baseDate)).getNextJieQi();
@@ -89,32 +95,69 @@ export default function JieQi() {
   //   console.log(currentJieQi.getName());
 
   return (
-    <div className='w-screen h-screen flex justify-center items-center'>
+    <div className='w-screen h-screen flex justify-center items-stretch bg-green-200'>
+      {/* skip previous  */}
       <div
-        className='h-screen w-[4%] flex items-center bg-slate-400'
+        className='h-screen w-[4%] flex justify-center items-center bg-slate-400 cursor-pointer'
         onClick={() => goToPrevJieQi()}
       >
         <BiSkipPrevious size={30} />
       </div>
 
-      <div className='w-full h-full bg-green-200 grid grid-cols-5'>
-        <div className='col-span-3'>
-          <div className=''>{currentJieQi}</div>
-          <div>{}</div>
+      <div className='w-full h-screen flex flex-col justify-start'>
+        <div className='w-full h-[45%] grid grid-cols-5'>
+          <div className='col-span-3 flex justify-center items-center'>
+            <p className='text-[210px] ml-[-200px] font-mashanzhang'>
+              {currentJieQi}
+            </p>
+          </div>
+
+          <div className='col-span-2 text-center flex justify-center items-center'>
+            <div className='flex flex-col justify-center items-center'>
+              <p>节气 anterior:</p>
+              <p>{prevSentence}</p>
+              <p>próximo 节气:</p>
+              <p>{nextSentence}</p>
+            </div>
+          </div>
         </div>
 
-        <div className='col-span-2'>
-          <div className=''>
-            <p>上一节气：</p>
-            <p>{prevSentence}</p>
-            <p>下一节气：</p>
-            <p>{nextSentence}</p>
+        <div className='w-full h-[55%] grid grid-cols-3'>
+          <div className='col-span-1 flex justify-center items-center'>
+            <div className='flex justify-center items-center'>
+              <p style={{ writingMode: 'vertical-rl' }}>床前明月光</p>
+              <p style={{ writingMode: 'vertical-rl' }}>疑是地上霜</p>
+              <p>(poema)</p>
+            </div>
+          </div>
+
+          <div className='col-span-1 flex justify-center items-center'>
+            <div className='flex justify-center items-center'>
+              <GiFlowerPot size={250} color='grey' />
+            </div>
+          </div>
+
+          <div className='col-span-1'>
+            <p className='p-6'>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. At quod
+              veniam necessitatibus accusamus harum doloribus ad, deleniti
+              maiores, ducimus perferendis dolores minus tempora est! Esse ullam
+              minima molestias sequi voluptas ipsum reiciendis, facilis
+              voluptate quas doloribus quos vel. Aliquam, aperiam quia laborum a
+              ut unde alias ipsum soluta totam delectus consequuntur deserunt
+              voluptate sunt cupiditate distinctio eius? Voluptate, possimus
+              amet!
+              <div className='text-end'>
+                <Link href={'#'}>...leia mais</Link>
+              </div>
+            </p>
           </div>
         </div>
       </div>
 
+      {/* skip next */}
       <div
-        className='h-screen w-[4%] flex items-center bg-slate-400'
+        className='h-screen w-[4%] flex justify-center items-center bg-slate-400 cursor-pointer'
         onClick={() => goToNextJieQi()}
       >
         <BiSkipNext size={30} />
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,20 @@
-import '@/styles/globals.css'
+import { useRouter } from 'next/router';
+import { Ma_Shan_Zheng } from '@next/font/google';
+import '@/styles/globals.css';
+
+const mashanzhang = Ma_Shan_Zheng({
+  subsets: ['latin'],
+  weight: '400',
+  variable: '--mashanzhang-font',
+  display: 'swap',
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  return (
+    <main className={`${mashanzhang.variable}`}>
+      <Component {...pageProps} key={router.asPath} />
+    </main>
+  );
 }
