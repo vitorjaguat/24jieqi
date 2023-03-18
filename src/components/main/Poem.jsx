41e3baa72933deb64b368ec697ad4d170a4f41d3
Vitor Butkus <vitorbutkus@gmail.com>
2023-03-18 19:28:48 -0300

    add key to Poem map

--- src/components/main/Poem.jsx
+++ src/components/main/Poem.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 
 export default function Poem({ current }) {
   const { t } = useTranslation('terms');
+  const { t: t1 } = useTranslation('common');
   const [poemArr, setPoemArr] = useState([]);
 
   useEffect(() => {
@@ -18,27 +19,41 @@ export default function Poem({ current }) {
   }, [current, t, setPoemArr]);
 
   return (
-    <div className='md:grid md:grid-cols-5 md:w-full md:h-full md:bg-green-100 items-center'>
-      <div className='md:w-full md:h-full md:col-span-2 flex justify-center items-center'>
-        <div className='w-full md:h-[70%] bg-green-100 flex justify-center items-center px-10 pt-16 md:p-20 md:pl-0 overflow-hidden'>
-          <div className='flex justify-center items-center'>
-            {poemArr.map((p) => (
-              <p
-                className='font-mashanzhang text-xl'
-                style={{ writingMode: 'vertical-rl' }}
-              >
-                {p}
-              </p>
-            ))}
+    <div className='w-full bg-green-100 pt-20'>
+      <div className='px-6 md:px-20'>
+        <h1 className='text-5xl text-center pb-10 font-bold font-mashanzhang tracking-wider'>
+          {t1('poem.poem')}
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
+          delectus est enim et mollitia, accusantium atque iste quas
+          necessitatibus, iusto labore corporis esse magnam ad cupiditate
+          expedita facilis laudantium laborum.
+        </p>
+      </div>
+      <div className='md:grid md:grid-cols-5 md:w-full md:h-full md:bg-green-100 items-center'>
+        <div className='md:w-full md:h-full md:col-span-2 flex justify-center items-center'>
+          <div className='w-full md:h-[70%] bg-green-100 flex justify-center items-center px-10 pt-16 md:p-20 md:pl-0 overflow-hidden'>
+            <div className='flex justify-center items-center'>
+              {poemArr.map((p, i) => (
+                <p
+                  key={i}
+                  className='font-mashanzhang text-xl'
+                  style={{ writingMode: 'vertical-rl' }}
+                >
+                  {p}
+                </p>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className='md:w-full md:col-span-3 flex justify-center items-center'>
-        <div className='w-full h-full bg-green-100 '>
-          <div className='p-6 py-16 md:max-w-[700px] mx-auto md:px-20 bg-gradient-to-t md:bg-gradient-to-l from-[#ffffff95] md:from-[#ffffff] to-transparent'>
-            <div className=''>
-              <p className=''>{t(`${current}.poem.now`)}</p>
+        <div className='md:w-full md:col-span-3 flex justify-center items-center'>
+          <div className='w-full h-full bg-green-100 '>
+            <div className='p-6 py-16 md:max-w-[700px] mx-auto md:px-20 bg-gradient-to-t md:bg-gradient-to-l from-[#ffffff95] md:from-[#ffffff] to-transparent'>
+              <div className=''>
+                <p className=''>{t(`${current}.poem.now`)}</p>
+              </div>
             </div>
           </div>
         </div>
