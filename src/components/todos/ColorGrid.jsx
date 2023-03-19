a63d5034d57c869c6603fc8fb905f694cb140a03
Vitor Butkus <vitorbutkus@gmail.com>
2023-03-19 15:44:55 -0300

    add jieqi name pinyin and translations on terms, reorganize /todos/Main.jsx

---
+++ src/components/todos/ColorGrid.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'next-i18next';
+import { GiGinkgoLeaf } from 'react-icons/gi';
+import Link from 'next/link';
+
+export default function ColorGrid({ current }) {
+  const { t } = useTranslation('terms');
+  const { t: t1 } = useTranslation('common');
+  const [hexArr, setHexArr] = useState([]);
+  const [nameArr, setNameArr] = useState([]);
+
+  useEffect(() => {
+    let hexData = [];
+    let nameData = [];
+
+    for (let i = 1; i <= 16; i++) {
+      let hex = t(`${current}.colors.${i}.hex`);
+      let name = t(`${current}.colors.${i}.name`);
+      hexData.push(hex);
+      nameData.push(name);
+    }
+    setHexArr([...hexData]);
+    setNameArr([...nameData]);
+  }, [current, setHexArr, setNameArr]);
+
+  console.log(hexArr, nameArr);
+
+  return (
+    <div className='w-full'>
+      <Link href='#colors' scroll={false}>
+        <div className='grid grid-cols-4 w-fit'>
+          {hexArr.map((hex, i) => (
+            <div key={i} className='w-6 h-6' style={{ backgroundColor: hex }} />
+          ))}
+        </div>
+      </Link>
+    </div>
+  );
+}
--- src/components/todos/Main.jsx
+++ src/components/todos/Main.jsx
@@ -1,17 +1,6 @@
 import { useTranslation } from 'next-i18next';
 import { useEffect, useState } from 'react';
-import ColorGrid from '../main/ColorGrid';
-
-const DUMMYcolors = {
-  立春: ['#FFF799', '#FFEE6F', '#ECD452', '#B6A014'],
-  雨水: ['#F9D3E3', '#ECB0C1', '#DD7694', '#A76283'],
-  惊蛰: ['#BA5B49', '#A64036', '#9E2A22', '#7C191E'],
-};
-// const DUMMYcolors = {
-//   立春: '#FFF799',
-//   雨水: '#F9D3E3',
-//   惊蛰: '#BA5B49',
-// };
+import ColorGrid from './ColorGrid';
 
 export default function Main({ jieqi }) {
   const { t } = useTranslation('terms');
@@ -31,18 +20,28 @@ export default function Main({ jieqi }) {
       style={{ backgroundColor: bgColor }}
     >
       <div className='w-[90%] md:max-w-[1200px] h-[80%] flex flex-col bg-[#00000020] rounded-xl'>
-        <div className='w-full h-full grid grid-cols-4'>
-          <div className='h-full col-span-1 flex flex-col items-center justify-start py-10 mx-auto'>
+        <div className='w-full h-full grid grid-cols-3'>
+          <div className='col-span-2 flex flex-col justify-center items-center'>
+            <p className='text-[120px] md:text-[150px] lg:text-[200px] font-mashanzhang'>
+              {jieqi}
+            </p>
+            {t(`${jieqi}.name-py`) !== '' ? (
+              <p className='italic'>{t(`${jieqi}.name-py`)}</p>
+            ) : (
+              ''
+            )}
+            {t(`${jieqi}.name-py`) !== '' ? (
+              <p className='md:text-lg'>{t(`${jieqi}.name-trans`)}</p>
+            ) : (
+              ''
+            )}
+          </div>
+
+          <div className='h-full col-span-1 flex flex-col items-center justify-end py-10 mx-auto'>
             <ColorGrid current={jieqi} />
           </div>
 
-          <div className='col-span-2 flex justify-center items-center'>
-            <p className='text-[100px] md:text-[150px] lg:text-[200px] font-mashanzhang text-center'>
-              {jieqi}
-            </p>
-          </div>
-
-          <div className='col-span-1 flex items-end justify-center py-10'></div>
+          {/* <div className='col-span-1 flex items-end justify-center py-10'></div> */}
         </div>
       </div>
     </div>
